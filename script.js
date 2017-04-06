2890e27fec28b4d85da724f99e80e93daf454889
DianaEromosele <diana.ozemebhoya@gmail.com>
2017-04-06 09:45:47 -0400

    Push up splash page for dog app

---
+++ script.js
@@ -0,0 +1,17 @@
+$(function() { 
+	$(".get-started-form").submit(function(event){
+	  event.preventDefault();
+	  $(".module")[0].style.display = "flex";
+	})
+
+	$(".close-module").click(function(event){
+		event.preventDefault();
+		$(".module")[0].style.display = "none";
+	})
+
+	$(window).click(function(event){
+		if ($(".module")[0].style.display = "flex") {
+			$(".module")[0].style.display = "none";
+		}
+	})
+})

e95941b49f6e347a3ad2f46dfaf38223c1263293
DianaEromosele <diana.ozemebhoya@gmail.com>
2017-05-09 11:34:02 -0400

    Clean up CSS to make it more responsive

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ $(function() {
 	$(".get-started-form").submit(function(event){
 	  event.preventDefault();
 	  $(".module")[0].style.display = "flex";
-	})
+	});
 
 	$(".close-module").click(function(event){
 		event.preventDefault();
 		$(".module")[0].style.display = "none";
-	})
+	});
 
 	$(window).click(function(event){
 		if ($(".module")[0].style.display = "flex") {
 			$(".module")[0].style.display = "none";
 		}
-	})
+	});
 })
